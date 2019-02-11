c134e3288030fda962144ad79dffed9cb1792e2b
Dron2200 <adovgalkpi@gmail.com>
2019-02-11 15:17:37 +0200

    Delete HW3_1.py

--- HW3_1.py
+++
@@ -1,8 +0,0 @@
-print("This app check and compare your name with mine ")
-username = input("please write your name in English(remember that name must start with capital letter): ")
-
-if username in("Andrey", "Andriy", "Andrew"):
-    print("you have the same name as I have ")
-elif username is not None or username != "":
-    print("Please, write your name correctly")
-else: print("your name is: ", username, "and you must be happy that we have different names")
